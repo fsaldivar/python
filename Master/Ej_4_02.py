d20638679c0e451873c2f5e3e1153f97cd44cf63
fsaldivar <fermax@gmail.com>
2019-10-09 21:47:26 -0500

    Ejercicio 4_02 del curso Master de Udemy

---
+++ Master/Ej_4_02.py
@@ -0,0 +1,18 @@
+'''
+Ejercicio 2
+
+Algoritmo que pida un número y diga si es positivo, negativo o 0.
+'''
+n1=int(input("Intruduce un numero: "))
+
+
+if n1>0:
+    print("El numero {} es POSITIVO".format(n1))
+
+if n1<0:
+    print("El numero {} es NEGATIVO".format(n1))
+
+if n1==0:
+
+    print("El numero {} es CERO".format(n1))
+    
