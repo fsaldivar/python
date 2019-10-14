adf6bc585f589092c3d2927f5b5cf82947f32630
fsaldivar <fermax@gmail.com>
2019-10-13 21:25:17 -0500

    Ejercicio 6 del cap 4

---
+++ Master/Ej_4_06.py
@@ -0,0 +1,12 @@
+'''
+Ejercicio 6
+Programa que lea una cadena por teclado
+y compruebe si es una letra mayúscula.
+'''
+
+
+cad = input("Introduce una cadena:")
+if cad == cad.upper():
+	print("La cadena es mayúsculas")
+else:
+	print("La cadena no es mayúsculas")
