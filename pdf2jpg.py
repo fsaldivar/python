a50fcca3c250fbdebd5971271edb98c4c409eee8
Fernando Saldivar Ceron <42698132+fsaldivar@users.noreply.github.com>
2020-07-17 20:43:20 -0500

    pdf to jpg using python
    Convertir archivos pdf a jpg usando python

---
+++ pdf2jpg.py
@@ -0,0 +1,16 @@
+import os
+from pdf2image import convert_from_path
+
+pdf_dir = r"C:\FSC\Linkedin Learning\certificados"
+os.chdir(pdf_dir)
+
+for pdf_file in os.listdir(pdf_dir):
+
+    if pdf_file.endswith(".pdf"):
+
+        pages = convert_from_path(pdf_file, 300)
+        pdf_file = pdf_file[:-4]
+
+        for page in pages:
+
+            page.save("%s-page%d.jpg" % (pdf_file,pages.index(page)), "JPEG")
